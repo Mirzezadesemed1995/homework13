64f861a39364a4afca6dc090a8446439d8964f56
Mirzezadesemed1995 <samedmirzezade@gmail.com>
2023-09-27 00:30:31 +0300

    added new document

--- 2/src/script.js
+++ 2/src/script.js
@@ -1,14 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const inputField = document.getElementById("inputField");
-
-    inputField.addEventListener("keydown", function(event) {
-        if (event.key === "Backspace") {
-            event.preventDefault();
-            inputField.value += " ";
-        } else if(event.key === " ") {
-            let sonveziyyet = inputField.value.split('');
-            sonveziyyet.pop();
-            inputField.value = sonveziyyet.join('');
-        }
-    });
-});
+ 
+    inputField.addEventListener("keydown", function (event) {
+       if (event.key === "Backspace") {
+          event.preventDefault();
+          inputField.value += " ";
+       } else if (event.key === " ") {
+          let lastStatusOnInput = [];
+ 
+          function spaceButton() {
+             lastStatusOnInput = inputField.value.split("");
+             lastStatusOnInput.pop();
+             inputField.value = lastStatusOnInput.join("");
+          }
+ 
+          if (
+             inputField.value.split("")[inputField.value.split("").length - 1] ==
+             " "
+          ) {
+             spaceButton();
+          }
+ 
+          spaceButton();
+       }
+ 
+       if (inputField.value == " ") {
+          inputField.value = "";
+       }
+    });
+ });
